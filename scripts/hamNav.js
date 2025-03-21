fb6fcde85702df24969b2bc23290eda72540a531
Argie <argiedc888@gmail.com>
2025-03-21 16:16:28 +0800

    finish updating my home page for week 03

---
+++ scripts/hamNav.js
@@ -0,0 +1,24 @@
+modeBtn.addEventListener("click", () => {
+    if (modeBtn.textContent.includes("🕶️")){
+        main.style.background = "black";
+        main.style.color = "white";
+        main.querySelector("h1").style.color = "white";
+        modeBtn.textContent = "🔆";
+    }
+
+    else {
+        main.style.background = "white";
+        main.style.color = "black";
+        main.querySelector("h1").style.color = "black";
+        modeBtn.textContent = "🕶️";
+    }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.getElementById("menu-button");
+    const menu = document.getElementById("menu");
+
+    menuButton.addEventListener("click", () => {
+        menu.classList.toggle("show");
+    });
+});
